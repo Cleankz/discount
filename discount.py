c887b1e3dcf807e48417ee6bc927b7274ab00b1f
Cleankz <92687535+Cleankz@users.noreply.github.com>
2021-12-13 19:52:00 +0300

    Create discount.py

---
+++ discount.py
@@ -0,0 +1,30 @@
+def MaximumDiscount(N, price):
+    if N == 3:
+        min = price[0]
+        for i in range(len(price)):
+            if min >= price[i]:
+                min = price[i]
+        return min
+    elif N > 3:
+        xchange = True 
+        while(xchange):
+            xchange = False
+            for i in range(len(price)-1):
+                if price[i] < price[i+1]:
+                    price[i], price[i+1] = price[i+1], price[i]
+                    xchange = True
+        free_item = N // 3
+        min_sum = 0
+        for i in range(free_item):
+            min = price[0]
+            for j in range(free_item+1):
+                if len(price) == 3:
+                    min =price[2]
+                    break
+                if j != 3 and min >= price[j]:
+                    min = price[j]
+            min_sum = min_sum + min
+            del price[0:3]
+            if len(price) < 2:
+                break
+        return min_sum
